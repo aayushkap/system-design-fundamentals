9f08964616a85ba6cafbb25b9b0aac88018245ca
Aayush Kapoor <akap@vitronic.com>
2025-08-14 13:49:06 +0400

    Repository Restructuring

--- http2/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI, Request
-
-app = FastAPI()
-
-@app.get("/")
-async def root(request: Request):
-    http_version = request.scope.get("http_version")
-    print(f"Incoming request over HTTP/{http_version}")
-    return {"message": f"Hello from HTTP/{http_version}!"}
